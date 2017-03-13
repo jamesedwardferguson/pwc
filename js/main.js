aba4ce2a299c3d5ecd14efaff1cfb38cac02c048
jamesedwardferguson <jamesedwardferguson78@gmail.com>
2017-03-13 11:37:01 +1100

    Add Responsive CSS and finished styling

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
-console.log("Hello World");
+$(document).ready(function() {
+    console.log("%cCode Test by James Ferguson", "background-color:gainsboro; font-size:15px; color: dodgerblue;");
 
-$
+    $(".buttons").on("click", function() {
+        console.log("css is the best");
+    });
+    $("#submitbutton").on("click", function() {
+        console.log("submit button has been pressed");
+    });
+
+});
